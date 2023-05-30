da85f51d89f7421449e439d94e852232d8ea0cee
Kondakov-Nik <kondakovnikita03@yandex.ru>
2023-05-30 11:19:42 +0900

    addedfiles

---
+++ Data_Base/DataWork.cs
@@ -0,0 +1,94 @@
+﻿// База данных работиников озон
+// author Kondakov N.S
+
+// todo иконка
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Data_Base //определение пространства имен Data_Base
+{
+    public class DataWork //классс для работы с базой
+    {
+        // коллекция workers объектов типа WorkersOzon
+        public ObservableCollection<WorkersOzon> workers;
+
+
+        // конструктор класса 
+        public DataWork()
+        {
+            workers = new ObservableCollection<WorkersOzon>();
+        }
+
+        // добавления нового работника в коллекцию workers
+        public void AddNewWorker(int id_worker, string name, int age, string post, double salary)
+        {
+            // создание нового объекта типа WorkersOzon
+            WorkersOzon new_worker = new WorkersOzon(id_worker, name, age, post, salary);
+
+            // добавление нового работника в коллекцию
+            workers.Add(new_worker);
+        }
+
+
+        // сохранить базу данных в файл
+        public void SaveDB(string name)
+        {
+            // использование StreamWriter для записи данных в файл с заданным именем
+            using (StreamWriter sw = new StreamWriter(name, false, System.Text.Encoding.Unicode))
+            {
+                foreach (WorkersOzon s in workers)
+                {
+                    sw.WriteLine(s.ToString());
+                }
+            }
+        }
+
+        // открыть базу данных из файла
+        public void OpenFile(string name_file)
+        {
+            // проверка, существует ли файл
+            if (!System.IO.File.Exists(name_file))
+                throw new Exception("Файл не существует");
+
+            // проверка, если в коллекции workers уже есть данные, удаляем их
+            if (workers.Count != 0)
+                DeleteDB();
+
+            // использование StreamReader для чтения данных из файла
+            using (StreamReader sw = new StreamReader(name_file))
+            {
+                while (!sw.EndOfStream)
+                {
+                    string str = sw.ReadLine();
+
+                    // разделение строки на отдельные символы используя разделитель "|"
+                    String[] dataFromFile = str.Split(new String[] { "|" },
+                        StringSplitOptions.RemoveEmptyEntries);
+
+                    // извлечение значений из строки и создание нового работника
+                    int id_worker = Convert.ToInt32(dataFromFile[0]);
+                    string name = dataFromFile[1];
+                    int age = Convert.ToInt32(dataFromFile[2]);
+                    string post = dataFromFile[3];
+                    double salary = Convert.ToDouble(dataFromFile[4]);
+                    AddNewWorker(id_worker, name, age, post, salary);
+                }
+            }
+        }
+
+        // удалить базу данных
+        public void DeleteDB()
+        {
+            workers.Clear();
+        }
+    }
+}
---
+++ Data_Base/WorkersOzon.cs
@@ -0,0 +1,106 @@
+﻿// База данных работиников озон
+// author Kondakov N.S
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Data_Base //определение пространства имен Data_Base
+{
+    // Класс работники озона
+    public class WorkersOzon // todo убррать _
+    {
+        int id_worker;     // Id
+        string name;       // Имя
+        int age;       // Возраст
+        string post;      // Должность
+        double salary;     // Зарплата
+
+        // конструктор класса
+        public WorkersOzon(int id_worker, string name, int age, string post, double salary)
+        {
+            this.id_worker = id_worker;
+            this.name = name;
+            this.age = age;
+            this.post = post;
+            this.salary = salary;
+        }
+
+        public string Name
+        {
+            get //Вернуть Имя
+            {
+                return name;
+            }
+
+            set //Задать Имя
+            {
+                name = value;
+            }
+        }
+
+        public int Id_worker
+        {
+            get //Вернуть Id
+            {
+                return id_worker;
+            }
+            set //Задать Id
+            {
+                id_worker = value;
+            }
+        }
+
+        public int Age
+        {
+            get //Вернуть возраст
+            {
+                return age;
+            }
+
+            set //Задать возраст 
+            {
+                age = value;
+            }
+        }
+
+        public string Post
+        {
+            get //Вернуть должность
+            {
+                return post;
+            }
+
+            set //Задать должность
+            {
+                post = value;
+            }
+        }
+
+        public double Salary
+        {
+            get //Вернуть зарплату
+            {
+                return salary;
+            }
+
+            set //Задать зарплату 
+            {
+                salary = value;
+            }
+        }
+
+        // Переопределение метода ToString. Каждый объект в языке С# получает метод ToString, который возращает строковое представление данного объекта.
+        // При создании пользовательского класса необходимо переопределить метод, чтобы передать информацию о типе
+        public override string ToString() // Преобразовать в строку
+        {
+            return id_worker + "|" + name + "|" + age + "|" +
+                post + "|" + salary;
+        }
+
+    }
+}
